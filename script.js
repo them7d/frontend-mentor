81f52492519525fa0e1b54c95a38283df6483540
Mohammad Zaid <mz188646@gmail.com>
2023-10-16 00:13:50 +0300

    fix bug in mobile screens

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var menuBar = document.querySelector(".icon-menu-bars"),
       closeBtn = document.querySelector('.close-menu-button'),
       menuContainer = document.querySelector(".menu-container");
-closeBtn.addEventListener("click", () => { 
-      menuContainer.classList.add("sm:hidden");
+closeBtn.addEventListener("click", () => {
+      console.log("clicked");
+      menuContainer.classList.add("xs:hidden");
 });
 menuBar.addEventListener("click", () => {
-      menuContainer.classList.remove("sm:hidden");
+      console.log("clicked");
+      menuContainer.classList.remove("xs:hidden");
 });
